fe7884b5e60d96f77789cb2c6c08eaeb39960d8c
Nolan <dongnguyenvie@gmail.com>
2022-09-18 18:49:32 +0700

    feat.UI upgrade layout ui for room detail

--- src/routes/rooms/[id]/index@fullscreen.svelte
+++ src/routes/rooms/[id]/index@fullscreen.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+	import Fa from 'svelte-fa';
+	import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 	import { page } from '$app/stores';
 	import { srcObject } from '$lib/@shared/directives/src-object.directive';
 	import { initRoomEvent } from '$lib/@shared/socket/events';
 	import Button from '$lib/components/Button.svelte';
-	import { room, socketState } from '$lib/state';
+	import { room } from '$lib/state';
 	import { onDestroy } from 'svelte';
 	import { derived, get } from 'svelte/store';
 	import * as process from 'process';
@@ -82,48 +84,60 @@
 {/if}
 
 {#if $accessable}
-	<section class="flex flex-col justify-between relative h-full max-h-screen">
-		<section class="flex justify-center py-2">
-			<div>
-				<h2>My socketId {$mySocketId}</h2>
-				<div class="scroll-m-9">
-					all users: {$usersId.join(', ')}
+	<div class="flex flex-row justify-between relative h-full max-h-screen">
+		<section class="flex flex-col justify-between w-full flex-shrink">
+			<section class="flex justify-center py-2 ">
+				<div>
+					{#if !$myMedia?.mediaStream}
+						<Button className="bg-main-500 rounded-lg hover:bg-main-800" onClick={handleOpenCam}>
+							open cam
+						</Button>
+					{/if}
+
+					{#if $myMedia?.mediaStream}
+						<Button className="bg-main-500 rounded-lg hover:bg-main-800" onClick={handleOffCam}>
+							off cam
+						</Button>
+					{/if}
+
+					{#if $myMedia?.audioStream}
+						<Button className="bg-main-500 rounded-lg hover:bg-main-800" onClick={handleOffMic}>
+							off mic
+						</Button>
+					{/if}
+					{#if !$myMedia?.audioStream}
+						<Button className="bg-main-500 rounded-lg hover:bg-main-800" onClick={handleOpenMic}>
+							open mic
+						</Button>
+					{/if}
+
+					<Button className="bg-main-500 rounded-lg hover:bg-main-800" onClick={handleChat}>
+						send text stream
+					</Button>
 				</div>
-				{#if !$myMedia?.mediaStream}
-					<Button className="bg-main-500 rounded-lg hover:bg-main-800" onClick={handleOpenCam}>
-						open cam
-					</Button>
-				{/if}
+			</section>
 
+			<section class="h-full overflow-hidden">
+				<div class={`flex justify-center items-center bg-slate-700 w-full h-full `}>
+					{#if !!$clientSelected?.isVideo}
+						<div class="bg-black w-full h-full">
+							<video
+								use:srcObject={nonNullAssert($clientSelected?.mediaStream)}
+								autoplay
+								class="w-full h-full scale-x-[-1] object-contain"
+							>
+								<track kind="captions" src="" />
+							</video>
+						</div>
+					{/if}
+				</div>
+			</section>
+
+			<section class="fixed right-0 top-1/2">
 				{#if $myMedia?.mediaStream}
-					<Button className="bg-main-500 rounded-lg hover:bg-main-800" onClick={handleOffCam}>
-						off cam
-					</Button>
-				{/if}
-
-				{#if $myMedia?.audioStream}
-					<Button className="bg-main-500 rounded-lg hover:bg-main-800" onClick={handleOffMic}>
-						off mic
-					</Button>
-				{/if}
-				{#if !$myMedia?.audioStream}
-					<Button className="bg-main-500 rounded-lg hover:bg-main-800" onClick={handleOpenMic}>
-						open mic
-					</Button>
-				{/if}
-
-				<Button className="bg-main-500 rounded-lg hover:bg-main-800" onClick={handleChat}>
-					send text stream
-				</Button>
-			</div>
-		</section>
-
-		<section class="h-full overflow-hidden">
-			<div class={`flex justify-center items-center bg-slate-700 w-full h-full `}>
-				{#if !!$clientSelected?.isVideo}
-					<div class="bg-black w-full h-full">
+					<div class="bg-black w-[200px] h-[200px]">
 						<video
-							use:srcObject={nonNullAssert($clientSelected?.mediaStream)}
+							use:srcObject={nonNullAssert($myMedia.mediaStream)}
 							autoplay
 							class="w-full h-full scale-x-[-1] object-contain"
 						>
@@ -131,77 +145,83 @@
 						</video>
 					</div>
 				{/if}
+			</section>
+
+			<section class="flex flex-nowrap items-end">
+				{#each $clients as client}
+					<section
+						class={`relative max-w-[96px] min-w-[60px] ml-1 overflow-hidden ${
+							client.socketId === $clientIdSelected ? 'border-red-800 border-4' : ''
+						}`}
+						title={client.socketId}
+					>
+						<div class="flex h-[33px] gap-1 items-end pb-1">
+							{#each $watchersMap[client.socketId] || [] as watcher}
+								<span
+									class={`w-[16px] h-[16px] overflow-hidden rounded-full inline-flex ${
+										client.socketId == watcher.socketId
+											? 'border-2 w-[20px] h-[20px] rounded-sm border-red-600'
+											: ''
+									}`}
+								>
+									<img
+										class="block object-cover w-full h-full"
+										src={client.avatar}
+										alt={client.socketId}
+									/>
+								</span>
+							{/each}
+						</div>
+
+						<div class={`relative  cursor-pointer`} on:click={handleFocusOn(client.socketId)}>
+							<img
+								class="block object-cover w-full h-full"
+								src={client.avatar}
+								alt={client.socketId}
+							/>
+							<div class={`absolute bottom-0 bg-green-700`}>
+								{#if client.isVideo}
+									<p>Video: on</p>
+								{/if}
+								{#if client.isAudio}
+									<p>audio: on</p>
+								{/if}
+								{#if client.socketId === $mySocketId}
+									<p>me</p>
+								{/if}
+							</div>
+						</div>
+					</section>
+				{/each}
+			</section>
+		</section>
+
+		<section class="w-[320px]">
+			<div class="flex flex-col justify-between border w-full h-full">
+				<div class="h-[45px] bg-slate-300" />
+				<div class="flex-grow bg-slate-400" />
+				<div class="h-[113px] bg-slate-500 flex flex-col p-[10px] pt-0">
+					<div class="h-[30px] pt-2">
+						<div class="bg-red-400 w-full h-full" />
+					</div>
+					<div class="relative flex flex-grow">
+						<textarea />
+						<span class="flex items-center justify-center flex-grow bg-main-500 text-white"
+							><Fa icon={faPaperPlane} /></span
+						>
+					</div>
+				</div>
 			</div>
 		</section>
+	</div>
+{/if}
 
-		<section class="fixed right-0 top-1/2">
-			{#if $myMedia?.mediaStream}
-				<div class="bg-black w-[200px] h-[200px]">
-					<video
-						use:srcObject={nonNullAssert($myMedia.mediaStream)}
-						autoplay
-						class="w-full h-full scale-x-[-1] object-contain"
-					>
-						<track kind="captions" src="" />
-					</video>
-				</div>
+{#if $accessable}
+	<div aria-hidden="true" class="hidden">
+		{#each $clientsAudio as media}
+			{#if media.audioStream}
+				<audio controls autoplay loop use:srcObject={nonNullAssert(media.audioStream)} />
 			{/if}
-		</section>
-
-		<section class="flex flex-nowrap items-end">
-			{#each $clients as client}
-				<section
-					class={`relative max-w-[96px] min-w-[60px] ml-1 overflow-hidden ${
-						client.socketId === $clientIdSelected ? 'border-red-800 border-4' : ''
-					}`}
-					title={client.socketId}
-				>
-					<div class="flex h-[33px] gap-1 items-end pb-1">
-						{#each $watchersMap[client.socketId] || [] as watcher}
-							<span
-								class={`w-[16px] h-[16px] overflow-hidden rounded-full inline-flex ${
-									client.socketId == watcher.socketId
-										? 'border-2 w-[20px] h-[20px] rounded-sm border-red-600'
-										: ''
-								}`}
-							>
-								<img
-									class="block object-cover w-full h-full"
-									src={client.avatar}
-									alt={client.socketId}
-								/>
-							</span>
-						{/each}
-					</div>
-
-					<div class={`relative  cursor-pointer`} on:click={handleFocusOn(client.socketId)}>
-						<img
-							class="block object-cover w-full h-full"
-							src={client.avatar}
-							alt={client.socketId}
-						/>
-						<div class={`absolute bottom-0 bg-green-700`}>
-							{#if client.isVideo}
-								<p>Video: on</p>
-							{/if}
-							{#if client.isAudio}
-								<p>audio: on</p>
-							{/if}
-							{#if client.socketId === $mySocketId}
-								<p>me</p>
-							{/if}
-						</div>
-					</div>
-				</section>
-			{/each}
-		</section>
-
-		<div aria-hidden="true" class="hidden">
-			{#each $clientsAudio as media}
-				{#if media.audioStream}
-					<audio controls autoplay loop use:srcObject={nonNullAssert(media.audioStream)} />
-				{/if}
-			{/each}
-		</div>
-	</section>
+		{/each}
+	</div>
 {/if}
