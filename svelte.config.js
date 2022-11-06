04c39396f1a4cf992682d069be67d86dc3d3e6d0
Nolan <dongnguyenvie@gmail.com>
2022-11-06 23:20:23 +0700

    feat.breaking-change update build config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
-// import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +12,7 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({}),
 		alias: {
 			$houdini: './$houdini'
 		}
