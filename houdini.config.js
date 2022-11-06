b0780ce26be7e5442a3d28b6be1e69bf292b45d5
Nolan <dongnguyenvie@gmail.com>
2022-11-06 23:54:47 +0700

    feat.core update endpoint for app

--- houdini.config.js
+++ houdini.config.js
@@ -3,7 +3,7 @@ const config = {
 	apiUrl: 'http://localhost:7001/graphql',
 	plugins: {
 		'houdini-svelte': {
-			client: './src/client'
+			client: './src/lib/@shared/graphql/client.ts'
 		}
 	},
 	schemaPath: './src/lib/@shared/graphql/schema.graphql'
